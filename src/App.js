a62bd43bc10510c5d46ca2b2d63548177180c60a
roryellis <rdellis1@gmail.com>
2020-06-11 13:50:03 -0500

    Created component files. App.js state and routes added.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const specialQueriesInitial = {
+    category : null,
+    daily_special : null,
+    special_day : null,
+    limited_time : null,
+    start_date : null,
+    end_date : null
+  }
+  const restaurantQueriesInitial = {
+    cuisine : null,
+    dining_options : null,
+    city : null,
+  }
+
+  const [specialQueries, setSpecialQueries] = useState(specialQueriesInitial);
+  const [restaurantQueries, setRestaurantQueries] = useState(restaurantQueriesInitial);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className='App'>
+			<AppHeader className='App-header' />
+			<Switch>
+				<Route exact path='/'>
+					<Redirect to='/specials' />
+				</Route>
+				<Route
+					path='/specials'
+					render={() => {
+						return (
+							<Specials
+								specialQueries={specialQueries}
+								setSpecialQueries={setSpecialQueries}
+								specialQueriesInitial={specialQueriesInitial}
+							/>
+						);
+					}}
+				/>
+				<Route
+					path='/restaurants'
+					render={() => {
+						return (
+							<Restaurants
+								restaurantQueries={restaurantQueries}
+								setRestaurantQueries={setRestaurantQueries}
+								restaurantQueriesInitial={restaurantQueriesInitial}
+							/>
+						);
+					}}
+				/>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
